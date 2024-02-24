244b1609a1a9843b865ee75b9d64df9c45837f1f
Kunal Pandhare <kunalpn7@gmail.com>
2024-02-24 23:55:40 +0530

    counter added

--- EventEaseFront/src/app/pages/home/home.component.ts
+++ EventEaseFront/src/app/pages/home/home.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
+export class HomeComponent implements OnInit {
+  counters!: NodeListOf<HTMLDivElement>;
+  speed = 200;
 
+  ngOnInit() {
+    this.counters = document.querySelectorAll<HTMLDivElement>(".count");
 
-export class HomeComponent {
-  ngOnInit(): void {
-    // Clear localStorage when the HomePage component is loaded
-    localStorage.clear();
+    this.counters.forEach((counter) => {
+      const updateCount = () => {
+        const target = parseInt(counter.getAttribute("data-target") || "0", 10);
+        const count = parseInt(counter.innerText, 10);
+        const increment = Math.trunc(target / this.speed);
+
+        if (count < target) {
+          counter.innerText = (count + increment).toString();
+          setTimeout(updateCount, 1);
+        } else {
+          counter.innerText = target.toString();
+        }
+      };
+      updateCount();
+    });
   }
 }
