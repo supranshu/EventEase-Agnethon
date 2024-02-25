1cfc1da0c0164aab5dccfe39db812c4192f9d711
SUPRANSHU SINGH <ss445198@gmail.com>
2024-02-25 18:41:00 +0530

    Tracked changes

--- EventEaseFront/src/app/app-routing.module.ts
+++ EventEaseFront/src/app/app-routing.module.ts
@@ -8,11 +8,7 @@ import { Dashboard2Component } from './pages/dashboard2/dashboard2.component';
 import { AddcommitteeComponent } from './pages/addcommittee/addcommittee.component';
 import { ViewcommitteeComponent } from './pages/viewcommittee/viewcommittee.component';
 import { AddeventsComponent } from './pages/addevents/addevents.component';
-<<<<<<< HEAD
-import { ContactComponent } from './pages/contact/contact.component';
-=======
 import { Dashboard3Component } from './pages/dashboard3/dashboard3.component';
->>>>>>> 47a890d610f9d6841c9c9c1d425855e5c99f7b64
 
 const routes: Routes = [
   {
